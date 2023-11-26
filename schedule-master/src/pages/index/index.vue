dbd656681485b89a9a4bbb89dce383ce0aa560cd
12faker <140673971+12faker@users.noreply.github.com>
2023-11-26 18:40:35 +0800

    更更index.vue

--- schedule-master/src/pages/index/index.vue
+++ schedule-master/src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
   <view>
     <view
-      ><button class="a" @tap="phoneWH()">获取信息</button>
+      ><button class="a" @tap="phoneWH()">获取机型信息</button>
       <button class="b" @tap="phoneWH()">点击</button></view
     >
   </view>
